882a1e69a05af8520158b44786067970c8ef4cf8
Michael Meadows <michael.meadows@insight.com>
2024-02-21 16:42:16 -0500

    with terraform yay

--- setup.sh
+++ setup.sh
@@ -7,12 +7,16 @@ GRAY='\033[0;90m'
 NC='\033[0m' # No Color
 
 printf "${GREEN}Prerequisites${NC}\n"
-PRINTF "${GRAY}Adding Repository for Functions Core Tools${NC}"
+
+printf "${GRAY}Required Packages${NC}"
+sudo apt install unzip
+
+printf "${GRAY}Adding Repository for Functions Core Tools${NC}"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 
-PRINTF "${GRAY}Adding Repository for .Net SDK${NC}"
+printf "${GRAY}Adding Repository for .Net SDK${NC}"
 declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -24,7 +28,11 @@ Pin: origin "packages.microsoft.com"
 Pin-Priority: 1001
 EOL'
 
-PRINTF "${GRAY}Getting Everything Current${NC}"
+printf "${GRAY}Adding Repository for Terraform${NC}\n"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+printf "${GRAY}Getting Everything Current${NC}"
 sudo apt update
 sudo apt upgrade -y
 
@@ -39,6 +47,9 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 nvm install --lts
 
+# Terraform
+sudo apt install terraform
+
 printf "${GREEN}VSCode${NC}\n"
 code --install-extension ms-dotnettools.vscode-dotnet-runtime
 code --install-extension ms-dotnettools.csdevkit
@@ -46,6 +57,7 @@ code --install-extension ms-vscode.vscode-node-azure-pack
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension gruntfuggly.todo-tree
+code --install-extension HashiCorp.terraform
 
 printf "${GREEN}Global Git settings${NC}\n"
 git config --global init.defaultBranch main
