7049b43e3fe38dbc52dce6b96fb0a6eda3495470
Michael Meadows <michaelmeadows@gmail.com>
2024-02-21 11:32:36 -0500

    Ready

--- setup.sh
+++ setup.sh
@@ -1,35 +1,41 @@
 #!/bin/sh
 
 echo Prerequisites
-# sudo apt-get upgrade
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-# sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
-# sudo apt-get update
+sudo apt-get upgrade
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
 
 echo .Net 6
-# sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get install -y dotnet-sdk-6.0
 
 echo Functions
-# sudo apt-get install azure-functions-core-tools-4
+sudo apt-get install azure-functions-core-tools-4
 
 echo Node.js
-# wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# source ~/.bashrc
-# nvm install --lts
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
 
 echo VSCode
-# code --install-extension ms-dotnettools.vscode-dotnet-runtime
-# code --install-extension ms-dotnettools.csdevkit
-# code --install-extension ms-vscode.vscode-node-azure-pack
-# code --install-extension ms-vscode-remote.remote-containers
-# code --install-extension davidanson.vscode-markdownlint
-# code --install-extension gruntfuggly.todo-tree
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension gruntfuggly.todo-tree
 
 echo The Code
 mkdir ~/temp
 cd ~/temp
 git init
 git remote add origin https://github.com/microsoft/WhatTheHack.git
-git pull origin master
-cp ~/temp/015-Serverless/Student/Resources ~/src/wth
+git pull origin master --depth 1
+cp ~/temp/015-Serverless/Student/Resources ~/src/wth -r
+
+echo Oh my ZSH
+sudo apt install fonts-powerline
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+omz theme set agnoster
