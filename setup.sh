5b416015505b5b795b50693ec44409faeff7c806
Michael Meadows <michaelmeadows@gmail.com>
2024-02-21 13:34:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,4 +58,4 @@ echo "${GREEN}Oh my ZSH${NC}\n"
 sudo apt install fonts-powerline
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-omz theme set agnoster
+omz theme set agnoster | bash
